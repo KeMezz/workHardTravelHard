06ffe61890db1617279d9418b6bcbc9f058673f2
KeMezz <88423849+KeMezz@users.noreply.github.com>
2022-02-15 12:35:21 +0900

    색상 변경

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.nav}>Work</Text>
+        <Text style={styles.nav}>Travel</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {},
+  nav: {
+    color: "#fff",
   },
 });
