a130a9baadde5556fb2d59871132170680959f48
정형진 <pnescene@icloud.com>
2022-02-20 12:59:09 +0900

    헤더 생성

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
+  const [isActive, setIsActive] = useState(false);
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <View style={styles.header}>
-        <Text style={styles.nav}>Work</Text>
-        <Text style={styles.nav}>Travel</Text>
+        <TouchableHighlight onPress={() => console.log("pressed")}>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableHighlight>
+        <Text style={styles.btnText}>Travel</Text>
       </View>
     </View>
   );
@@ -16,12 +27,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
-  header: {},
-  nav: {
-    color: "#fff",
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    // justifyContent: "space-between",
+  },
+  btnText: {
+    color: theme.gray,
+    flex: 1,
+    fontSize: 36,
+    fontWeight: "600",
   },
 });
